3cb917500aebe40ec0054bfea86d45d989202612
SiennaB124 <04310613@nebraska.edu>
2026-01-23 01:48:39 +0000

    Completed lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,22 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Sienna Bonner
+#Date:1/22/26
+#Assignment:Lab 1 
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  name = input("what is your name?\n")
   #Use the user's name in the program.
-
+  print("hello " + name)
   #Ask the user for their age.
-
+  age = input("what is your age?\n")
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
-
+  birthYear = (2025 - int(age))
+  print("You were born in " + str(birthYear))
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
